25b10d204d978923333441f410e63115c3e4b2d1
Roovy Shapiro <roovyshapiro@gmail.com>
2021-06-10 22:55:46 -0400

    Sub, Tipper, Recipients are all in tables

--- ChaintipStats/reddit_tips/views.py
+++ ChaintipStats/reddit_tips/views.py
@@ -24,11 +24,9 @@ def main(request):
 
     all_tips_ordered = all_tips.order_by('-created_datetime')
     all_stats['start_date'] = all_tips_ordered.last().created_datetime
-    top_senders = all_tips.values_list('sender').annotate(sender_count=Count('sender')).order_by('-sender_count')
-    all_stats['top_senders'] = top_senders[0:16]
+    all_stats['all_senders'] = all_tips.values_list('sender').annotate(sender_count=Count('sender')).order_by('-sender_count')
     all_tips_receivers = all_tips.filter(~Q(returned = True))
-    top_receivers = all_tips_receivers.values_list('receiver').annotate(receiver_count=Count('receiver')).order_by('-receiver_count')
-    all_stats['top_receivers'] = top_receivers[0:16]
+    all_stats['all_receivers'] = all_tips_receivers.values_list('receiver').annotate(receiver_count=Count('receiver')).order_by('-receiver_count')
     all_stats['all_subs'] = all_tips.values_list('subreddit').annotate(subreddit_count=Count('subreddit')).order_by('-subreddit_count')
 
     all_stats['bch_price'] = bch_prices.first().price_format
