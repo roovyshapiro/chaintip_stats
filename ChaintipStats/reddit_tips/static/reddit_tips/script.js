da2c9d1896a796990364c4fcca3bca35ccad8582
Roovy Shapiro <roovyshapiro@gmail.com>
2021-06-10 22:37:27 -0400

    DataTables implementation

--- ChaintipStats/reddit_tips/static/reddit_tips/script.js
+++ ChaintipStats/reddit_tips/static/reddit_tips/script.js
@@ -1,1 +1,6 @@
-console.log("JS is connected");
+//https://datatables.net/
+//Data Tables provides built in functionality such as
+//sorting, filtering & choosing how many rows to load
+$(document).ready( function () {
+    $('table.data_table').DataTable();
+} );
