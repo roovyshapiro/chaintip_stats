3cba2ae5fc2779ed91fad9c783cb82a7d022b3b5
Roovy Shapiro <roovyshapiro@gmail.com>
2021-06-30 22:18:39 -0400

    Fixed mobile view

--- ChaintipStats/ChaintipStats/settings.py
+++ ChaintipStats/ChaintipStats/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = credential_dict['django_secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.55.145']
 
 
 # Application definition
